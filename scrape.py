03934f96395ecbb53a5563b4b78c49db3981d5c1
Mark Gelfius <mgelfius@purdue.edu>
2018-06-29 19:05:42 -0400

    Prints out schedule for whole season and has titles seperating each week. However, it takes like 30 seconds to run

--- scrape.py
+++ scrape.py
@@ -3,17 +3,14 @@ import urllib.request
 import urllib.error
 from bs4 import BeautifulSoup
 from datetime import datetime
-
 currentYear = str(datetime.today().year)
-weekNum = 1
 url = "http://www.nfl.com/schedules/" + currentYear + "/REG"
-page = urllib.request.urlopen(url + str(weekNum))
-soup = BeautifulSoup(page, "html.parser")
 
-matchup = soup.find_all("div", "list-matchup-row-team")
-for i in range(0, len(matchup)):
-    print(matchup[i].text.strip())
+for n in range(1, 18):
+    print('\033[95m' + "WEEK " + str(n) + " GAMES" + '\033[0m' + '\n')
+    page = urllib.request.urlopen(url + str(n))
+    soup = BeautifulSoup(page, "html.parser")
 
-
-
-
+    matchup = soup.find_all("div", "list-matchup-row-team")
+    for i in range(0, len(matchup)):
+        print(matchup[i].text.strip() + '\n')
