252718842228aec1274c32faca9443b655995e77
Mark Gelfius <mgelfius@purdue.edu>
2018-06-29 22:51:15 -0400

    Prints by date of game

--- scrape.py
+++ scrape.py
@@ -10,7 +10,11 @@ for n in range(1, 18):
     print('\033[95m' + "WEEK " + str(n) + " GAMES" + '\033[0m' + '\n')
     page = urllib.request.urlopen(url + str(n))
     soup = BeautifulSoup(page, "html.parser")
-
-    matchup = soup.find_all("div", "list-matchup-row-team")
+    matchup = soup.find_all(["li", "div"], ["schedules-list-date", "list-matchup-row-team"])
+    #matchup = soup.find_all("div", "list-matchup-row-team")
     for i in range(0, len(matchup)):
-        print(matchup[i].text.strip() + '\n')
+        if "Full Thursday Night" in matchup[i].text.strip():
+            s = matchup[i].text.strip().split("View")
+            print(s[0] + '\n')
+        else:
+            print(matchup[i].text.strip() + '\n')
